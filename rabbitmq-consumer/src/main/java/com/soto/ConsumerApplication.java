37e42687dcd7a634812de4e1df29b9a3e1380a2f
sotowang <sotowang@qq.com>
2019-08-06 09:19:57 +0800

    rabbitmq:consumer+producer

---
+++ rabbitmq-consumer/src/main/java/com/soto/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.soto;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
+
+
+
---
+++ rabbitmq-consumer/src/main/java/com/soto/consumer/OrderReceiver.java
@@ -0,0 +1,43 @@
+package com.soto.consumer;
+
+import com.alibaba.fastjson.JSONObject;
+import com.rabbitmq.client.Channel;
+import com.soto.entity.Order;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+@Component
+public class OrderReceiver {
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "order-queue",durable = "true"),
+            exchange = @Exchange(value = "order-exchange",durable = "true",type = "topic"),
+            key = "order.*"
+    ))
+    @RabbitHandler
+    public void onOrderMessage(@Payload Order order,
+                               @Headers Map<String, Object> headers,
+                               Channel channel) throws IOException {
+
+//        System.out.println("Jorder ------------"+jOrder);
+
+        System.out.println("-----------收到消息,开始消费-----------");
+
+//        Order order = JSONObject.toJavaObject(jOrder, Order.class);
+        System.out.println("订单ID:  " + order.getId());
+
+        Long deliverTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+
+        //ACK
+        channel.basicAck(deliverTag, false);
+
+    }
+
+
+}
